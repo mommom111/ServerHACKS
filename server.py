cbbbfb44fbfc41a9b1c3f1ef7e51eaa786a35f10
Hideyasu <kutuzov1228@gmail.com>
2022-10-21 21:41:13 +0900

    feat: websocket server add broadcast

--- server.py
+++ server.py
@@ -1,17 +1,34 @@
 import asyncio
 import websockets
+from IPython import embed
+import nest_asyncio
+
+nest_asyncio.apply()
  
-async def hello(websocket):
-    name = await websocket.recv()
-    print(f"<<< {name}")
- 
-    greeting = f"Hello {name}!"
- 
-    await websocket.send(greeting)
-    print(f">>> {greeting}")
+clients = set()
+device_count = "0"
+
+async def broadcast():
+    while True:
+        for ws in clients:
+            global device_count
+            device_count
+            await ws.send(device_count)
+        await asyncio.sleep(1)
+
+asyncio.get_event_loop().create_task(broadcast())
+
+async def handler(websocket, path):
+    clients.add(websocket)
+    try:
+        async for msg in websocket:
+            global device_count
+            device_count = msg
+    finally:
+        clients.remove(websocket)
  
 async def main():
-    async with websockets.serve(hello, "localhost", 8765):
+    async with websockets.serve(handler, "localhost", 8765):
         await asyncio.Future()  # run forever
  
 if __name__ == "__main__":
